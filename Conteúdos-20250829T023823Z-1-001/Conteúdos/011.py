127e7f557d3c130de34805556072cf5d33855d80
andredalcorsi <andredalcorsi1@gmail.com>
2025-08-28 23:39:42 -0300

    exercises update

--- Conteúdos-20250829T023823Z-1-001/Conteúdos/011.py
+++
@@ -1,3 +0,0 @@
-#Escreva um programa que peça ao usuário para adivinhar um número entre 1 e 10 e continue pedindo até que o usuário acerte o número.
-#E no final, retorne também a quantidade de tentativas necessárias.
-
