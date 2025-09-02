40e230a72a4aced68de568b5b702868077cd87df
andredalcorsi <andredalcorsi1@gmail.com>
2025-09-02 17:02:25 -0300

    exercises update

--- 024.py
+++ 024.py
@@ -1,12 +1,25 @@
 #Usando tuplas, leia um número de 0 a 15, e retorne esse número escrito por extenso
+try: 
+    numeros = ('zero',
+            'um', 
+            'dois', 
+            'três', 
+            'quatro', 
+            'cinco', 
+            'seis', 
+            'sete', 
+            'oito', 
+            'nove', 
+            'dez', 
+            'onze', 
+            'doze', 
+            'treze', 
+            'quatorze', 
+            'quinze')
 
+    n = int(input('Digite um número: '))
 
+    print(numeros[n])
 
-
-def numeros('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'):
-    numeros.replace(0,'Zero')
-
-
-n = int(input('Digite um número: '))
-
-print(numeros(n))
+except IndexError:
+    print('Número inválido, digite um número entre 0 e 15')
