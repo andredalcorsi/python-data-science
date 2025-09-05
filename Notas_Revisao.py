f91c565350a1462b702fd545b034958deae7ae16
andredalcorsi <andredalcorsi1@gmail.com>
2025-09-04 22:48:44 -0300

    exercises update

--- Notas_Revisao.py
+++ Notas_Revisao.py
@@ -269,5 +269,92 @@ list = [['joão', 56], ['Maria', 74], ['thiago', 5]]
 
 print(lista[0][0])
 
-for i in lista:
-    print(i[1])
+    for i in lista:
+        print(i[1])
+
+#Colunas
+
+nomes = []
+idades = []
+sexos = []
+Alunos = []
+
+
+for i in range(3):
+    nomes.append(input('Nome: '))
+    idades.append(int(input('Idade: ')))
+    sexos.append(input('Sexo[M/F]: ').strip().upper()[0])
+
+Alunos.append(nomes)
+Alunos.append(idades)
+Alunos.append(sexos)
+print(nomes)
+print(idades)
+print(sexos
+
+print(f'A lista final é {Alunos}')
+
+Alunos = [[],[],[]]
+
+for i in range(3):
+    Alunos[0].append(input('Nome: '))
+    Alunos[1].append(int(input('Idade: ')))
+    Alunos[2].append(input('Sexo[M/F]: ').strip().upper()[0])
+    print(Alunos)
+
+print(f'O Aluno mais velho é '
+      f'\n{Alunos[0][Alunos[1].index(max(Alunos[1]))]}')
+
+#Dicionários
+
+df = {'Nome': 'Luis Tatin', 'Idade':56}
+print(df['Nome'])
+df['Sexo'] = 'M'
+#del df['Idade']
+
+print(df)
+print(df.values())
+print(df.keys())
+print(df.items())
+
+df = [{'Marca': A, 'Modelo': 'X', 'Ano': 2000},
+      {'Marca': B, 'Modelo': 'Y', 'Ano': 2001},
+      {'Marca': C, 'Modelo': 'Z', 'Ano': 2002},]
+
+#print(df[1]['Marca'])
+
+carro = {}
+
+df = []
+
+for i in range(3):
+
+    carro['Marca'] = input('Marca: ')
+    carro['Modelo'] = input('Modelo: ')
+    carro['Ano'] = int(input('Ano: '))
+    df.append(carro.copy())
+print(df)
+
+#2
+
+df = { 'Marca': ['A', 'B', 'C'],
+      'Modelo': ['X', 'Y', 'Z']
+      'Ano'   : ['2000', '2001', '2001']}
+
+#print(df['Modelo'][1])
+
+Marca = []
+Modelo = []
+Ano = []
+df = {}
+
+for i in range(3):
+        Marca.append(input('Marca: '))
+        Modelo.append(input('Modelo: '))
+        Ano.append(int(input('Ano: ')))
+
+df['Marca'] = Marca[:]
+df['Modelo'] = Modelo [:]
+df['Ano'] = Ano[:]
+print(df)
+'''
