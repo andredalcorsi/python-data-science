85044258221115b9ff308d8d4948046e67104c2f
andredalcorsi <andredalcorsi1@gmail.com>
2025-09-16 17:29:30 -0300

    exercises update

--- Pandas/022.py
+++ Pandas/022.py
@@ -2,7 +2,7 @@
 
 1. Importe a Vendas_Incorreto - OK 
 
-2. Trate todos os dados incorretos
+2. Trate todos os dados incorretos - OK 
 
 3. Analise estatisticamente o arquivo
 
@@ -17,3 +17,11 @@ df = pd.read_excel(file_path)
 df.dropna(axis=1, how='all', inplace=True)
 df.dropna(axis=0, how='all',inplace=True)
 
+df.rename(columns={'Preco': 'Preço de Venda'}, inplace=True)
+df.rename(columns={'Devolucao': 'Devolução'}, inplace=True)
+
+print(df.head())
+print(df.info())
+
+
+print(f'A média de Descontos é igual a \n: ', df['Desconto'].median())
